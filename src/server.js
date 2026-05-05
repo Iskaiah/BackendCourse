dd1a18784a4374a7e4f5b4559a31ce27ade8e154
Isaiah Katoboe <katoboeisaiah@gmail.com>
2026-05-05 16:28:15 +1100

    insert a script for development

--- src/server.js
+++ src/server.js
@@ -5,6 +5,6 @@ const app = express();
 const PORT = 5001;
 
 const server = app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+    console.log('Server is running on PORT ${PORT}');
 });
 
