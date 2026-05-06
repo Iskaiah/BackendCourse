314a1e7df2f969503ff3df3bcbd1c787e042493e
Isaiah Katoboe <katoboeisaiah@gmail.com>
2026-05-06 13:07:24 +1100

    feat: implement error handling and graceful shutdown in server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
+import { connectDB, disconnectDb } from "../config/db.js"; 
 
 // Import routes
 import movieRoutes from "./routes/movieRoutes.js";
 
 config();
+connectDB();
 
 const app = express();
 
@@ -18,5 +20,27 @@ app.listen(PORT, () => {
     console.log('Server is running on PORT ${PORT}');
 });
 
-// GET, POST, PUT, DELETE
-// http://localhost:5001/hello
+// Handle unhandled promise rejections (e.g., database connection issues)
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Rejection: ", err);
+    server.close( async () => {
+        await disconnectDB();
+        process.exit(1);
+    });
+});
+
+// Handle uncaught exceptions (e.g., programming errors)
+process.on("uncaughtException", async (err) => {
+    console.error("Uncaught Exception", err);
+    await disconnectDB();
+    process.exit(1);
+});
+
+//Graceful shutdown
+process.on("SIGTERM", async () => {
+    console.log("SIGTERM received, shutting down gracefully...");
+    server.close( async () => {
+        await disconnectDB();
+        process.exit(0);
+    });
+});
